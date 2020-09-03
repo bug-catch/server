b3b3e5b7bbc84e689ac35ce845647e9439ccd28d
Harry Merritt <harry@merritt.es>
2020-09-03 22:28:16 +0100

    style: setup gulp to apply prettier on all js files

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require("gulp");
+const prettier = require("gulp-prettier");
+
+/*
+ * Apply prettier to all js files
+ */
+gulp.task("default", () => {
+    return gulp
+        .src([
+            "./lib/**/*.js",
+            "./package.json",
+            "./.eslintrc.js",
+            "./gulpfile.js",
+        ])
+        .pipe(prettier({ editorconfig: true }))
+        .pipe(gulp.dest((file) => file.base));
+});
