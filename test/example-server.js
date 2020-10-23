5bfa567def5b3b909f197fc3b7eb9a1b11594974
Harry Merritt <harry@merritt.es>
2020-10-23 21:47:22 +0100

    test: add example server
    - loads config from .env file

--- test/example-server.js
+++ test/example-server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+require("dotenv").config();
+
 const bugcatch = require("../index");
 
 const server = express();
 server.use(express.json());
 
+server.use((req, res, next) => {
+    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.append("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 //  Applly bugcatch as middleware
-server.use( "/", bugcatch({ test: "wow" }) );
+server.use(
+    "/",
+    bugcatch({
+        mongodb: {
+            uri: process.env.BUGCATCH_MONGO_URI,
+            database: process.env.BUGCATCH_MONGO_DATABASE,
+        },
+    })
+);
 
 server.listen(process.env.PORT || 8000);
